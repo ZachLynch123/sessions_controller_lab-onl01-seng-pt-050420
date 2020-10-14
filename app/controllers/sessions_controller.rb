58722ae2c37469f0172945271741cc3758aabd4a
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-10-14 02:10:44 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
   
   def create 
     session[:username] = params[:username]
-        redirect_to '/'
+    redirect_to '/'
     
+  end
+  
+  def destroy
+    session.delete :username
   end
   
 end
